c1068de9fa7c92548303ff13941d6fc058ce7e49
Nikolai Pankov <pankoff2000@gmail.com>
2021-12-13 09:45:04 +0100

    JS-day-4-refactoring

--- JAVASCRIPT/Sélecteur de Photos/main.js
+++ JAVASCRIPT/Sélecteur de Photos/main.js
@@ -1,30 +1,35 @@
 const liElements = document.querySelectorAll('li');
 const selectAll = document.querySelector('#selectAll');
 const deselectAll = document.querySelector('#deselectAll');
-const DIV = document.querySelector('#total em').textContent;
+const DIV = document.querySelector('#total em');
 
 function hoverClick() {
     this.classList.toggle('selected');
     counterPhotos()
 }
 
-function selectAllPotos() {
+function selectAllPhotos() {
     liElements.forEach((li) => li.classList.add('selected'));
     counterPhotos()
 }
 
-function deselectAllPotos() {
+function deselectAllPhotos() {
     liElements.forEach((li) => li.classList.remove('selected'));
     counterPhotos()
 }
 
 function counterPhotos() {
    const numberPhotos = document.querySelectorAll('.selected');
-   document.querySelector('#total em').textContent = numberPhotos.length;
+   if (numberPhotos.length === 0) {
+       DIV.style.color = 'red';
+   } else {
+       DIV.style.color = 'yellow';
+   }
+   DIV.textContent = numberPhotos.length;
 }
 
 liElements.forEach((li) => li.addEventListener('click', hoverClick));
-selectAll.addEventListener('click', selectAllPotos);
-deselectAll.addEventListener('click', deselectAllPotos);
+selectAll.addEventListener('click', selectAllPhotos);
+deselectAll.addEventListener('click', deselectAllPhotos);
 
 
