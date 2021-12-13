e9e29c42edf5a10b43b72ac2506609e6c771b1be
Nikolai Pankov <pankoff2000@gmail.com>
2021-12-13 15:07:10 +0100

    JS-day-5

--- JAVASCRIPT/Liste de courses DOM/main.js
+++ JAVASCRIPT/Liste de courses DOM/main.js
@@ -1,103 +1,62 @@
-'use strict';
-    let listProducts = [];
-    let inputToDelete,
-        btnDeleteAllProducts,
-        btnDelete,
-        popUp,
-        listElements;
+"use strict";
+let listProducts = [];
+let inputToDelete, btnDeleteAllProducts, btnDelete, popUp, listElements;
 
-    function addProduct(product) {
-        listProducts.push(product);
-    }
+function addProduct(product) {
+  listProducts.push(product);
+}
 
-    function findAndRemoveProduct(product) {
-        product = inputToDelete.value
-        let index = listProducts.indexOf(product);
+function findAndRemoveProduct(product) {
+  product = inputToDelete.value;
+  let index = listProducts.indexOf(product);
 
-        if (index != -1) {
-            listProducts.splice(index, 1);
-        } else {
-            alert('Ce product ne figure pas dans la liste');
-        }
-        displayList();
-        hidePopUp();
-    }
+  if (index != -1) {
+    listProducts.splice(index, 1);
+  } else {
+    alert("Ce product ne figure pas dans la liste");
+  }
+  displayList();
+  hidePopUp();
+}
 
-    function deleteAllProducts() {
-        listProducts = [];
-        displayList();
-    }
+function deleteAllProducts() {
+  listProducts = [];
+  displayList();
+}
 
-    function displayList() {
-        listElements.innerHTML = '';
-        listProducts.forEach((el) => listElements.innerHTML += `<li>${el}</li>` )
-        document.querySelector('section h2').innerHTML = `Il y a ${listProducts.length} elements`;
-    }
+function displayList() {
+  listElements.innerHTML = "";
+  listProducts.forEach((el) => (listElements.innerHTML += `<li>${el}</li>`));
+  document.querySelector(
+    "section h2"
+  ).innerHTML = `Il y a ${listProducts.length} elements`;
+}
 
-    function getValue() {
-        const item = document.querySelector('#toAdd').value;
-        addProduct(item);
-        displayList();
-    }
+function getValue() {
+  const item = document.querySelector("#toAdd").value;
+  addProduct(item);
+  displayList();
+}
 
-    function hidePopUp() {
-        popUp.classList.add('hide');
-    }
-    
-    document.addEventListener('DOMContentLoaded', function() {
-         inputToDelete = document.querySelector('#toDelete')
-         btnDeleteAllProducts = document.querySelector('#delete');
-         btnDelete = document.querySelector('#btnDelete');
-         popUp = document.querySelector('#popup');
-         listElements = document.querySelector('.list');
-        
-        btnDelete.addEventListener('click', findAndRemoveProduct);
+function hidePopUp() {
+  popUp.classList.add("hide");
+}
 
-        document.querySelector('#deleteOne').addEventListener('click', function() {
-            popUp.classList.remove('hide');
-        });
+document.addEventListener("DOMContentLoaded", function () {
+  inputToDelete = document.querySelector("#toDelete");
+  btnDeleteAllProducts = document.querySelector("#delete");
+  btnDelete = document.querySelector("#btnDelete");
+  popUp = document.querySelector("#popup");
+  listElements = document.querySelector(".list");
 
-        document.querySelector('span').addEventListener('click', hidePopUp);
-        
-        btnDeleteAllProducts.addEventListener('click', deleteAllProducts);
-        document.querySelector('#submit').addEventListener('click', getValue)
-    })
+  btnDelete.addEventListener("click", findAndRemoveProduct);
 
+  document.querySelector("#deleteOne").addEventListener("click", function () {
+    popUp.classList.remove("hide");
+  });
 
-    
-    // function addProduct(event) {
-    //     event.preventDefault();
-    //     // listProducts.push(input.value);
-    //     listElements.innerHTML += `<li>${input.value}</li>`
-    // }
+  document.querySelector("span").addEventListener("click", hidePopUp);
 
-    // function deleteProduct() {
-    //     const liElements = listElements.childNodes;
-    //     liElements.forEach((el) => {
-    //         if (el.textContent === inputToDelete.value) {
-    //             console.log('delete one', el.textContent)
-    //         }
-    //     })
-    // }
-
-    // function deleteAllProducts() {
-    //     listElements.innerHTML = ''
-    // }
-
-    // function getValue() {}
-    
-
-    // btnAdd.addEventListener('click', addProduct);
-
-    // document.querySelector('#deleteOne').addEventListener('click', function() {
-    //     popUp.classList.remove('hide');
-    // });
-    
-    // btnDeleteAllProducts.addEventListener('click', deleteAllProducts);
-
-    // document.querySelector('span').addEventListener('click', function() {
-    //     popUp.classList.add('hide');
-    // })
-
-    // btnDelete.addEventListener('click', deleteProduct);
-
+  btnDeleteAllProducts.addEventListener("click", deleteAllProducts);
+  document.querySelector("#submit").addEventListener("click", getValue);
+});
