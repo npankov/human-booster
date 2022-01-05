e47945fdbdb4476ff78384edc851440f02b0f61b
Nikolai Pankov <pankoff2000@gmail.com>
2022-01-05 22:22:02 +0100

    ANGULAR-day-1

--- ANGULAR/J1/exercices_tableaux/exercise-3.js
+++
@@ -1,9 +0,0 @@
-function exsTree() {
-  let numbers = [1, 2, 3, 4];
-
-  const smallest = Math.min(...numbers);
-  const largest = Math.max(...numbers);
-  console.log('min =', smallest, 'max =', largest);
-}
-
-exsTree();
