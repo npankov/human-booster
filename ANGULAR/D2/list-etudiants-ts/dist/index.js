7fe14c56ae9d52b5f3d49b0f8527f585974e9ddb
Nikolai Pankov <pankoff2000@gmail.com>
2022-01-07 14:47:36 +0100

    ANGULAR-day-3-list-student-ts-averageAge

--- ANGULAR/D2/list-etudiants-ts/dist/index.js
+++ ANGULAR/D2/list-etudiants-ts/dist/index.js
@@ -13,11 +13,18 @@ function renderStudent(student, students) {
         deleteStudent(student, divStudent, students);
     });
     content.append(divStudent);
+    console.log(averageAge(students));
 }
 function renderAllStudents(students) {
+    const content = document.querySelector('#content');
+    const divAverageAge = document.createElement('div');
     for (let student of students) {
         renderStudent(student, students);
     }
+    divAverageAge.innerHTML = `
+    <strong>Average Age:</strong> ${averageAge(students)}
+  `;
+    content.append(divAverageAge);
 }
 function deleteStudent(student, divStudent, students) {
     divStudent.remove();
@@ -25,6 +32,9 @@ function deleteStudent(student, divStudent, students) {
     if (positionStudent != -1) {
         students.splice(positionStudent, 1);
     }
+}
+function averageAge(students) {
+    return students.reduce((acc, student) => acc + student.age, 0) / students.length;
 }
 renderAllStudents(students);
 //# sourceMappingURL=index.js.map
--- ANGULAR/D2/list-etudiants-ts/src/index.ts
+++ ANGULAR/D2/list-etudiants-ts/src/index.ts
@@ -17,12 +17,22 @@ function renderStudent(student: Student, students: Student[]) {
     deleteStudent(student, divStudent, students);
   })
   content.append(divStudent);
+  console.log(averageAge(students));
 }
 
 function renderAllStudents(students: Student[]) {
+  const content = (document.querySelector('#content') as HTMLDivElement);
+  const divAverageAge = document.createElement('div');
+
   for (let student of students) {
     renderStudent(student, students)
   }
+
+  divAverageAge.innerHTML = `
+    <strong>Average Age:</strong> ${averageAge(students)}
+  `;
+  
+  content.append(divAverageAge);
 }
 
 function deleteStudent(student: Student, divStudent: HTMLDivElement, students: Student[]) {
@@ -32,6 +42,10 @@ function deleteStudent(student: Student, divStudent: HTMLDivElement, students: S
   if (positionStudent != -1) {
     students.splice(positionStudent, 1)
   }
+}
+
+function averageAge(students: Student[]) {
+  return students.reduce((acc, student) => acc + student.age, 0) / students.length;
 }
 
 renderAllStudents(students)
