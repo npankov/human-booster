0edf5cff4fccf6972defc9232f6fd3bf43c92639
Nikolai Pankov <pankoff2000@gmail.com>
2022-01-07 09:19:50 +0100

    ANGULAR-day-2-list-student-ts

---
+++ ANGULAR/D2/list-etudiants-ts/src/index.ts
@@ -0,0 +1,35 @@
+type Student = {
+  name: string,
+  lastName: string,
+  age: number
+}[];
+
+let students: Student = [
+  {
+    name: 'Nikolai',
+    lastName: 'Pankov',
+    age: 31
+  },
+  {
+    name: 'Guillaume',
+    lastName: 'Perroy',
+    age: 26
+  },
+  {
+    name: 'Emma',
+    lastName: 'Segeral',
+    age: 24
+  },
+  {
+    name: 'Luka',
+    lastName: 'Del Rosso',
+    age: 23
+  }
+]
+
+
+students.map((student) => {
+  const div = (document.querySelector('#content') as HTMLDivElement);
+
+  div.innerHTML += `<div><strong>Name:</strong> ${student.name} <strong>Last Name:</strong> ${student.lastName} <strong>Age:</strong> ${student.age}</div>`
+})
