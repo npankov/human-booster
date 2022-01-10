e03b408dcc99e1dd5ed620a1e093bfb044396d5a
Nikolai Pankov <pankoff2000@gmail.com>
2022-01-10 21:25:21 +0100

    ANGULAR-day-4-get-city-from-index-api

--- ANGULAR/D4/tp-webpack/src/demoFunction.ts
+++ ANGULAR/D4/tp-webpack/src/demoFunction.ts
@@ -1,3 +1,7 @@
-export function testWebPack() {
-    console.log('Hello WebPack')
-};
+export function getLink() {
+    let link: string = 'https://geo.api.gouv.fr/communes?codePostal=';
+    const index: string = (document.querySelector('#index') as HTMLInputElement).value;
+    console.log(link + index)
+    return link + index;
+}
+
--- ANGULAR/D4/tp-webpack/src/index.ts
+++ ANGULAR/D4/tp-webpack/src/index.ts
@@ -1,3 +1,14 @@
-import { testWebPack } from "./demoFunction";
+import { getLink } from "./demoFunction";
 
-testWebPack()
+const form = document.querySelector('form');
+const result = document.querySelector('#result') as HTMLParagraphElement;
+
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const link = getLink();
+  fetch(link)
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => data.map((el: any) => result.innerHTML = el.nom));
+})
